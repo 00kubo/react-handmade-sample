c188b9087cda13f5ff677dbbdb9b66e195a0af93
0okubo <ontama3390@gmail.com>
2019-11-06 01:08:51 +0900

    カウンターアプリ実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById('root')
-);
+function Result(props: any) {
+  return <h1>{props.count}</h1>;
+}
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  React.useEffect(() => {
+    document.title = 'Count : ${count}';
+  });
+
+  return (<div>
+    <Result count={count} />
+    <button onClick={handleClick}>Add</button>
+  </div>);
+};
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
